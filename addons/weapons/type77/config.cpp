e2b9a0fc9866a4697b87ba9a1979faf5dadd4ccf
Björn Dahlgren <bjorn@dahlgren.at>
2020-05-01 23:42:06 +0200

    Japanese weapons

---
+++ addons/weapons/type77/config.cpp
@@ -0,0 +1,31 @@
+class CfgPatches {
+    class anrop_fow_jsrs_weapons_type77 {
+        name = "anrop_fow_jsrs_weapons_type77";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"fow_weapons_c", "jsrs_soundmod_complete_edition"};
+        addonRootClass = "fow_weapons_c";
+        author = "Anrop";
+        url = "https://www.anrop.se";
+    };
+};
+
+class Mode_FullAuto;
+
+class CfgWeapons 
+{
+    class MGun;
+
+    class fow_w_type77_mg: MGun
+    {
+        class manual: MGun
+        {
+            class BaseSoundModeType;
+            class StandardSound: BaseSoundModeType
+            {
+                soundSetShot[] = {"jsrs_m2_vehicle_shot_soundset", "jsrs_ww2_mg_reverb_soundset"};
+            };
+        };
+    };
+};
